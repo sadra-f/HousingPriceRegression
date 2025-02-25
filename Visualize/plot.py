d96c9d5c349f0a355cb5a77d94e94a9786413a92
sadra-f <sadrafouladi@gmail.com>
2025-02-26 00:12:24 +0330

    updated to plot the loss value over generations

--- Visualize/plot.py
+++ Visualize/plot.py
@@ -1,9 +1,16 @@
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 from random import shuffle
 import multiprocessing
+import time
+import numpy as np
 
-def plot_on_process(X, Y, sub_w, sub_h, title=None):
+def plot_multi_column_on_process(X, Y, sub_w, sub_h, title=None):
     process = multiprocessing.Process(target=plot_multi_col, args=(X, Y, sub_w, sub_h, title))
+    process.start()
+    return process
+def plot_on_process(file_path, title=None):
+    process = multiprocessing.Process(target=plot_line, args=(file_path, title))
     process.start()
     return process
 
@@ -22,4 +29,17 @@ def plot_multi_col(X, Y, sub_w, sub_h, title=None):
         axis[i%sub_w, i%sub_h].scatter(X[col], Y, color=colors[i])
         axis[i%sub_w, i%sub_h].set_title(col)
         axis[i%sub_w, i%sub_h].set_ylabel('Price')
+    plt.show()
+
+def _update(i):
+    Y = np.loadtxt("tmp/loss_history.txt")[1:]
+    X = [i for i in range(len(Y))]
+    plt.cla()
+    plt.plot(X, Y)
+
+def plot_line(file_path , title=None):
+    Y = np.loadtxt(file_path)
+    X = [i for i in range(len(Y))]
+    ani = FuncAnimation(plt.gcf(), _update, interval=1000)
+    plt.tight_layout()
     plt.show()
